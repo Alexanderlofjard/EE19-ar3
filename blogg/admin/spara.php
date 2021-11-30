7a520bf91fb10f5b4a4d2da16a297fe405d96592
Alexanderlofjard <alexander.lofjard@elev.ga.ntig.se>
2021-11-30 13:55:10 +0100

    lite gott och blandat

---
+++ blogg/admin/spara.php
@@ -0,0 +1,16 @@
+<?php
+//Ta emot data
+$rubrik = filter_input(INPUT_POST, "rubrik", FILTER_SANITIZE_STRING);
+$meddelande = filter_input(INPUT_POST, "meddelande", FILTER_SANITIZE_STRING);
+
+
+
+//Om data finns...
+if ($rubrik && $meddelande) {
+$texten = "<h3>$rubrik</h3> <p>$meddelandez</p>";
+
+    file_put_contents("blogg.txt", $texten, FILE_APPEND);
+
+    echo "<p>meddelandet sparades</p>";
+}
+?>
